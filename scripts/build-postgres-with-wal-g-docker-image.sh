d1c32d0c212adc87352e2df9229a9eceefdae978
Ilia Anikeev <iaanikeev@10.211.17.250-vpn.dhcp.yndx.net>
2024-11-24 17:56:17 +0300

    remove unnecessary scripts

--- scripts/build-postgres-with-wal-g-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../"
-
-./scripts/build-wal-g-docker-image.sh
-
-docker build ./docker-image/postgres-with-wal-g -t postgres-with-wal-g-shmp
--- scripts/build-wal-g-docker-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../"
-
-docker build ./docker-image/wal-g/ -t wal-g-shmpg
