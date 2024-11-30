ecb60a0f5d1757700678d9b407a61a134295974f
Ilia Anikeev <iaanikeev@172.31.107.157-vpn.dhcp.yndx.net>
2024-12-01 02:42:53 +0300

    mount postgres config file

--- docker-image/postgres-with-wal-g/docker-entrypoint.sh
+++ docker-image/postgres-with-wal-g/docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+mkdir -p /etc/postgresql/ \
+    && cp /usr/local/share/postgresql/postgresql.conf.sample /etc/postgresql/postgresql.conf.tmpl \
+    && sed -ri "s/^#archive_mode = off/archive_mode = {{.Env.ARCHIVE_MODE}}/" /etc/postgresql/postgresql.conf.tmpl \
+    && sed -ri "s/^#archive_timeout = 0/archive_timeout = {{.Env.ARCHIVE_TIMEOUT}}/" /etc/postgresql/postgresql.conf.tmpl \
+    && sed -ri "s/^#archive_command = ''/archive_command = '\/wal-g wal-push %p'/" /etc/postgresql/postgresql.conf.tmpl \
+    && sed -ri "s/^#restore_command = ''/restore_command = '\/wal-g wal-fetch %f %p'/" /etc/postgresql/postgresql.conf.tmpl
+
+
 gomplate -f /etc/postgresql/postgresql.conf.tmpl -o /etc/postgresql/postgresql.conf
 
 /usr/local/bin/docker-entrypoint.sh "$@"
